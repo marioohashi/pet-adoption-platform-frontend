8863925369a2fc844b6b9fdff2a5f16ab1ccd774
marioohashi <mario.ohashi@gmail.com>
2026-08-13 18:32:17 -0300

    project created

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Button from './components/button';
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Responsible Pet Adoption Platform</h1>
+      <h2>Find Your Furry Friend</h2>
+      
+      <>
+        <Button /> 
+        <Button />
+      </>
+    </div>
+  )
+} 
---
+++ src/components/button.tsx
@@ -0,0 +1,9 @@
+const Button: React.FC = () => {
+  return (
+    <button>
+      Click me
+    </button>
+  );
+};
+
+export default Button;
